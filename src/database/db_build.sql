fb7c5ba42d3343dda0ad2d14fc108e68fdf44e19
tloth <toth.annar@gmail.com>
2019-04-11 12:53:19 +0100

    create db_buil.sql

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -1,7 +1,28 @@
 BEGIN;
 
+CREATE TABLE actions (
+    id SERIAL PRIMARY KEY,
+    action point VARCHAR(100) NOT NULL,
+    date DATE
+);
 
+INSERT INTO actions (action, date)
+VALUES
+    ('stop using gendered language when referring to the entire group (see: "guys")', 2019-03-08),
+    ('use Lost and Founders to keep track of action points and opinions', 2019-04-12);
 
+CREATE TABLE opinions (
+    id SERIAL PRIMARY KEY,
+    action_id INTEGER REFERENCES actions(id) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    opinion TEXT NOT NULL,
+    date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO opinions (action_id, name, opinion)
+VALUES
+    (1, 'Anna', 'I would rather we didn''t use "guys" when talking to everyone'),
+    (2, 'Kate', 'It might be fun!');
 
 
 
